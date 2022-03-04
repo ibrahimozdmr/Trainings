20574459454182ad348259d1dceac707a2900bbf
Halil İbrahim <43646661+ibrahimozdmr@users.noreply.github.com>
2022-03-05 01:29:13 +0300

    4 mart kodları

--- Ruby/koleksiyonlar.rb
+++ Ruby/koleksiyonlar.rb
@@ -29,3 +29,56 @@ puts "silme işlemi: #{dizim}"
 
 # aralık operatörü ile dizi içerisinde belli bir aralığa erişme
 puts "bir aralığa erişme: #{dizim[1...4]}"
+
+# Dizileri birleştirme (combining arrays)
+birinciDizi = [1, 2]
+ikinciDizi = [3, 4, 5, 6]
+beraber = birinciDizi + ikinciDizi # toplama operatörü ile birleştirdik.
+puts "birinciDizi + ikinciDizi : #{beraber}"
+
+# diziler arasındaki farkı da bir dizi yapabiliriz.
+birinciDizi = [5, 6, 7, 8, 9]
+ikinciDizi = [5, 6, 2, 9]
+beraber = birinciDizi - ikinciDizi # çıkarma operatörü birinci diziden varsa ikindi dizi elemanlarını çıkarıyor.
+puts "birinciDizi - ikinciDizi : #{beraber}"
+
+# diziyi aynı elemanlar ile eleman sayısını katlayabiliriz
+dizi = [3, 4, 5]
+dizi = dizi * 3 # aynı dizi elemanları 3'er tane oldu.
+puts "dizi * 3 : #{dizi}"
+
+# dizilerde boole işlemleri yapalım
+dizi1 = [32, 54, 12, 72, 67]
+dizi2 = [54, 32, 5]
+puts "dizi1 & dizi2 : #{dizi1 & dizi2}" # & operatörü diziler arasında ortak olanları ekrana bastırıyor.
+
+puts "dizi1 | dizi2 : #{dizi1 | dizi2}" # | operatörü dizileri, aynı eleman olmadan birleştiriyor.
+
+# elemanların yerinden oynaması
+a = [7, 8, 9]
+b = a.reverse # reverse fonsiyonu diziyi tersten ayarlıyor.
+puts "a: #{a}\nb(a'nın tersi): #{b}"
+
+a = [11, 12, 13]
+puts "\nters basılmış a: #{a.reverse}" # burada sadece a'nın tersini bastırdık.
+puts "a dizisi:        #{a}"
+puts "tersten kaydedilmiş a: #{a.reverse!}" # eğer sonuna ünlem bırakılırsa a'ya tersini atayacak. a değişip tersinden sıralanacak.
+puts "a dizisi:              #{a}"
+
+# dizilerde en çok kullanılan fonksiyonlar (bu fonksiyonlar sadece sayılar için değil metinsel ifadeler için de geçerli)
+dizi = [1, 2, 3, 4, 5, 3, 1, 5]
+puts "\ndizi: #{dizi}\ndizi'nin uzunluğu: #{dizi.length}" # size ve length fonksiyonları dizi uzunluğu döndürmek için kullanılır
+puts "dizi'nin uzunluğu: #{dizi.size}"
+puts "dizinin sıralı hali: #{dizi.sort}" # diziyi küçükten büyüğe sıralayarak yeni bir dizi döndürür
+puts "dizinin tekrarsız hali: #{dizi.uniq}" # diziden yinelenen elemanları kaldırarak yeni bir dizi döndürür
+puts "dizinin tekrarsız hali: #{dizi.uniq!}" # yinelenen eleman olmadan aynı diziyi değiştirir
+puts "#{dizi.freeze}" # dizinin değiştirilmesini önler
+puts "dizide 5 var mı: #{dizi.include? 5}" # yanı gelen parametre dizinin içersinde var mı diye kontrol eder
+puts "dizinin en küçüğü: #{dizi.min}" # dizinin en küçük değerini döndürüyor
+puts "dizinin en büyüğü: #{dizi.max}" # dizinin en büyük değerini döndürüyor
+
+# dizi üzerinde gezinme for döngüsü ile
+print "dizide gezinerek bastırma: "
+for x in dizi
+    print "#{x} "
+end
